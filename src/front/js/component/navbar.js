a21943a02c6db44185ae09354a1b5f7f2f150b5d
juan meisus <130049281+juanmeisus@users.noreply.github.com>
2023-12-07 20:37:14 +0000

    Incorporo la Navbar actualizada de Juan

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -21,25 +21,19 @@ export const Navbar = () => {
 			</div>
 			<div className="offcanvas-body navbar-background">
 				<ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-					<li className="nav-item">
-						<a className="nav-link active" aria-current="page" href="#">home</a>
+					<li >
+					<Link className="navbar-link" to={"/dashboard"}><i className="fa-solid fa-swatchbook" style={{ "color": "#f0f2f4" }}></i>Dashboard</Link>
+
 					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">Link</a>
+					<li >
+					<Link className="navbar-link" to={"/"}><i className="fa-solid fa-house-laptop" style={{ "color": "#f0f2f4" }}></i>Home</Link>
+	
 					</li>
-					<li className="nav-item dropdown">
-						<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							Dropdown
-						</a>
-						<ul className="dropdown-menu dropdown-menu-dark">
-							<li><a className="dropdown-item" href="#">Action</a></li>
-							<li><a className="dropdown-item" href="#">Another action</a></li>
-							<li>
-								<hr className="dropdown-divider" />
-							</li>
-							<li><a className="dropdown-item" href="#">Something else here</a></li>
-						</ul>
+					<li >
+					<Link className="navbar-link" to={"/login"}><i className="fa-solid fa-user" style={{ "color": "#f0f2f4" }}></i>Login</Link>
+
 					</li>
+					
 				</ul>
 				<form className="d-flex mt-3" role="search">
 					<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
