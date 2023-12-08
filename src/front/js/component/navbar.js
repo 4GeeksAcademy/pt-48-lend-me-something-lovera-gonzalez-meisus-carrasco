e08b2108c09fd62eb4f147fbed54c591c347e7f1
Augusto Carrasco <augustocarrascotoro@gmail.com>
2023-12-08 10:02:47 +0000

    Updated Navbar Background to darker blak"

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../../styles/shared.sass'
+import '../../styles/navbar.sass'
 
 export const Navbar = () => {
 	return (<>
-		<div className="d-flex flex-column align-items-center bg-dark vh-100  fixed-top rounded-2" style={{ "width": "6em" }}>
+		<div className="d-flex flex-column align-items-center navbar-background vh-100  fixed-top rounded-2" style={{ "width": "6em" }}>
 
 
 			<button className=" btn " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
@@ -14,12 +14,12 @@ export const Navbar = () => {
 			<Link to={"/"}><i className="fa-solid fa-house-laptop" style={{ "color": "#f0f2f4" }}></i></Link>
 			<Link to={"/login"}><i className="fa-solid fa-user" style={{ "color": "#f0f2f4" }}></i></Link>
 		</div>
-		<div className="offcanvas offcanvas-start text-bg-dark bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+		<div className="offcanvas offcanvas-start navbar-background" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
 			<div className="offcanvas-header">
 				<h5 className="offcanvas-title text-light" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
 				<button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 			</div>
-			<div className="offcanvas-body">
+			<div className="offcanvas-body navbar-background">
 				<ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
 					<li className="nav-item">
 						<a className="nav-link active" aria-current="page" href="#">home</a>
