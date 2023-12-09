b22a18f60b86d7483bdd8d81c9a8988a95f89c52
juan meisus <130049281+juanmeisus@users.noreply.github.com>
2023-12-09 17:33:42 +0000

    modificando al off canvas

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -10,42 +10,36 @@ export const Navbar = () => {
 			<div className=" d-flex flex-column align-items-center justify-content-between gap-5">
 
 				<button className=" btn " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-					<i className="fa-solid fa-bars" style={{ "color": "#f0f2f4" }}></i>
+					<i className="fa-solid fa-bars" ></i>
 				</button>
 				<div className="navbar-icons">
 
-					<Link to={"/dashboard"}><i className="fa-solid fa-swatchbook" style={{ "color": "#f0f2f4" }}></i></Link>
-					<Link to={"/"}><i className="fa-solid fa-house-laptop" style={{ "color": "#f0f2f4" }}></i></Link>
-					<Link to={"/login"}><i className="fa-solid fa-user" style={{ "color": "#f0f2f4" }}></i></Link>
-					<Link to={"/"}><i className="fa-solid fa-gear" style={{ "color": "#f0f2f4" }}></i></Link>
-					<Link to={"/"}><i className="fa-solid fa-bell" style={{ "color": "#f0f2f4" }}></i></Link>
+					<Link to={"/dashboard"}><i className="fa-solid fa-swatchbook" ></i></Link>
+					<Link to={"/"}><i className="fa-solid fa-house-laptop" ></i></Link>
+					<Link to={"/login"}><i className="fa-solid fa-user" ></i></Link>
+					<Link to={"/"}><i className="fa-solid fa-gear" ></i></Link>
+					<Link to={"/"}><i className="fa-solid fa-bell" ></i></Link>
 				</div>
 			</div>
 			<img className="navbar-profilepicture" src="https://picsum.photos/id/64/200/200" />
 
 		</div>
-		<div className="offcanvas offcanvas-start text-bg-dark bg-dark navbarcustom" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+		<div className="offcanvas offcanvas-start text-bg-dark bg-dark navbarcustom d-flex flex-column justify-content-between vh-100" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
 			<div className="offcanvas-header">
 				<h5 className="offcanvas-title text-light" id="offcanvasDarkNavbarLabel">Options</h5>
 				<button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 			</div>
-			<div className="offcanvas-body">
+			<div className="offcanvas-body ">
 
-				<ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-					<li >
-						<Link className="navbar-link" to={"/dashboard"}><i className="fa-solid fa-swatchbook" style={{ "color": "#f0f2f4" }}></i>Dashboard</Link>
+				<div className="navbar-offcanvas-icons">
 
-					</li>
-					<li >
-						<Link className="navbar-link" to={"/"}><i className="fa-solid fa-house-laptop" style={{ "color": "#f0f2f4" }}></i>Home</Link>
+					<Link className="navbar-link" to={"/dashboard"}><i className="fa-solid fa-swatchbook" ></i>Dashboard</Link>
+					<Link className="navbar-link" to={"/"}><i className="fa-solid fa-house-laptop" ></i>Home</Link>
+					<Link className="navbar-link" to={"/login"}><i className="fa-solid fa-user" ></i>Login</Link>
+					<Link className="navbar-link" to={"/"}><i className="fa-solid fa-gear" ></i>Settings</Link>
+					<Link className="navbar-link" to={"/"}><i className="fa-solid fa-bell" ></i>Notifications</Link>
+				</div>
 
-					</li>
-					<li >
-						<Link className="navbar-link" to={"/login"}><i className="fa-solid fa-user" style={{ "color": "#f0f2f4" }}></i>Login</Link>
-
-					</li>
-
-				</ul>
 				<form className="d-flex mt-3" role="search">
 					<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
 					<button className="btn btn-success" type="submit">Search</button>
