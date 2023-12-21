4ed6485b30dfc2099613dff14cdb13158b6c2116
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2023-12-21 10:46:05 +0000

    sketched market reposiory

---
+++ src/api/Repositories/market_repository.py
@@ -0,0 +1,24 @@
+from api.Models.market_model import Market
+from engine import session
+
+
+class MarketRepository:
+    @staticmethod
+    def get_market_list():
+        return
+    
+    @staticmethod
+    def get_market_by_id():
+        return
+    
+    @staticmethod
+    def add_market():
+        return
+    
+    @staticmethod
+    def update_market():
+        return
+    
+    @staticmethod
+    def delete_market():
+        return
