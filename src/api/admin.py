6ee7ec633c4069adc229c697ff21d6e862ef84e5
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2023-12-21 10:37:48 +0000

    correction to marketModel

--- src/api/admin.py
+++ src/api/admin.py
@@ -3,6 +3,7 @@ import os
 from flask_admin import Admin
 from .models import db
 from api.Models.user_model import User
+from api.Models.market_model import Market
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -13,6 +14,7 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Market, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
