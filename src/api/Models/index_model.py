e725cd72a4f30a7147e9c797dce9309c8a1ea013
AugustoCarrasco <augustocarrascotoro@gmail.com>
2023-12-24 13:21:34 +0100

    Configuracion para trabajo local

--- src/api/Models/index_model.py
+++ src/api/Models/index_model.py
@@ -1,22 +1,21 @@
 from api.models import db
 
+
 class Index(db.Model):
-    __tablename__ = 'indices'
+    __tablename__ = "indices"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable =False)
-    icon = db.Column(db.String, nullable =False)
-    currency= db.Column(db.String, nullable=False)
-    create_at =db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    icon = db.Column(db.String, nullable=False)
+    currency = db.Column(db.String, nullable=False)
+    create_at = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
-        return f'<index:{self.name}>'
-    
+        return f"<index:{self.name}>"
+
     def serialize(self):
         return {
-           'name': self.name,
-           'icon':self.icon,
-           'currency':self.currency,
-           'create_at':self.create_at
+            "name": self.name,
+            "icon": self.icon,
+            "currency": self.currency,
+            "create_at": self.create_at,
         }
-
-    
--- src/api/engine.py
+++ src/api/engine.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from api.models import db
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://gitpod:postgres@localhost:5432/example", echo=True)
+engine = create_engine("postgresql+psycopg2://postgres:1725@localhost:5432/postgres")
 db.Model.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
