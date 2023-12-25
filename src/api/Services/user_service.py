ce602623922cfc2ded36d2ba56f499d4138e735c
Augusto Carrasco <augustocarrascotoro@gmail.com>
2023-12-25 23:58:23 +0000

    Tailored User Service

--- src/api/Services/user_service.py
+++ src/api/Services/user_service.py
@@ -1,9 +1,10 @@
 from api.Repositories.user_repository import UserRepository
 from api.Services.HTTP_Status import HTTP_Status
 
+
 class UserService:
     @staticmethod
     def get_user():
-        raw_data =  UserRepository.get_list()
+        raw_data = UserRepository.get_list()
         serialized_data = list(map(lambda element: element.serialize(), raw_data))
-        return serialized_data
+        return serialized_data, HTTP_Status.OK
