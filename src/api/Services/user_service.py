792c0006057b04444d46999ec5bc6494d138b443
Augusto Carrasco <augustocarrascotoro@gmail.com>
2023-12-25 23:56:02 +0000

    User service touched

--- src/api/Services/user_service.py
+++ src/api/Services/user_service.py
@@ -1,6 +1,8 @@
-
+from api.Repositories.user_repository import UserRepository
 
 class UserService:
     @staticmethod
     def get_user():
-        pass
+        raw_data =  UserRepository.get_list()
+        serialized_data = list(map(lambda element: element.serialize(), raw_data))
+        return serialized_data
