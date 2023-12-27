79270ddba77e241ec4d17f6c408221340d0f39fe
Augusto Carrasco <augustocarrascotoro@gmail.com>
2023-12-27 11:13:14 +0000

    Propuesta Aliquindoi

--- src/front/js/component/dashboard/doughnut_chart.js
+++ src/front/js/component/dashboard/doughnut_chart.js
@@ -3,45 +3,45 @@ import * as d3 from "d3";
 
 const data = [
     {
-        label: 'Santander',
-        price: 3.80,
+        label: 'Ocio',
+        price: 40,
         color: '#4fa2ff'
     },
     {
-        label: 'Apple',
+        label: 'Trabajo',
         price: 80,
         color: '#0d715d'
     },
     {
-        label: 'Iberdrola',
-        price: 11.95,
+        label: 'Familia',
+        price: 50,
         color: '#ffd155'
     },
-    {
-        label: 'Inditex',
-        price: 35,
-        color: '#A200EA'
-    },
-    {
-        label: 'Aena',
-        price: 42,
-        color: '#e216EA'
-    },
-    {
-        label: 'BVA',
-        price: 25,
-        color: '#e216EA'
-    },
-    {
-        label: 'Unicaja',
-        price: 18,
-        color: '#e216EA'
-    },
-    {
-        label: 'Google',
-        price: 125,
-        color: '#e216EA'
-    },
+    // {
+    //     label: 'Inditex',
+    //     price: 35,
+    //     color: '#A200EA'
+    // },
+    // {
+    //     label: 'Aena',
+    //     price: 42,
+    //     color: '#e216EA'
+    // },
+    // {
+    //     label: 'BVA',
+    //     price: 25,
+    //     color: '#e216EA'
+    // },
+    // {
+    //     label: 'Unicaja',
+    //     price: 18,
+    //     color: '#e216EA'
+    // },
+    // {
+    //     label: 'Google',
+    //     price: 125,
+    //     color: '#e216EA'
+    // },
 ]
 
 export const Doughnut = () => {
@@ -56,7 +56,7 @@ export const Doughnut = () => {
         const outterRadius = d3.min([width, height]) / 2;
         // console.log(outterRadius)
 
-        const yScale = d3.scaleLinear([0, data.length], [height + margin.top, 0]);
+        const yScale = d3.scaleLinear([0, data.length], [(height + margin.top)/2, 0]);
         const colorScale = d3.scaleLinear().domain([d3.max(data.map(d => d.price)), d3.max(data.map(d => d.price)) / 2, d3.min(data.map(d => d.price))]).range(["#ff0000", "#ffa500", "#ffff00", "#008000", "#4fa2ff", "#4b0082", "#ee82ee"]);
         console.log(d3.extent(data.map(d => d.price)))
         console.log(colorScale(25))
@@ -73,7 +73,10 @@ export const Doughnut = () => {
         // .attr('height', height)
         // .attr('fill', 'green');
 
-        const pie = d3.pie().value((d) => d.price)
+        const pie = d3.pie()
+            .value((d) => d.price)
+            .startAngle(-Math.PI/2)
+            .endAngle(Math.PI/2)
 
         const data_ready = pie(data)
         // console.table(data_ready)
@@ -96,20 +99,19 @@ export const Doughnut = () => {
             .data(data.map(d => d.price))
             .attr('fill', data => `${colorScale(data)}`)
 
-        div
-            .append('text')
-            // .attr('transform', `translate(${300 / 4},${250 / 4})` )
-            .text(`${data.reduce((a, e) => a + e.price, 0)}€`)
-            .attr('stroke', 'white')
-            .attr("dx", "-1.8em")
-            .attr("dy", "0.2em")
-            .attr('fill', 'white')
-            .attr('style', 'font-size: 2em')
+        // div
+        //     .append('text')
+        //     .text(`${data.reduce((a, e) => a + e.price, 0)}€`)
+        //     .attr('stroke', 'white')
+        //     .attr("dx", "-1.8em")
+        //     .attr("dy", "0.2em")
+        //     .attr('fill', 'white')
+        //     .attr('style', 'font-size: 2em')
 
         const labels = div
             .append('g')
             .attr("transform", `translate(${300 / 2},-${250 / 2})`);
-        labels
+        const text = labels
             .selectAll('text')
             .data(d3.sort(data, (a, b) => d3.ascending(a.price, b.price)).map(d => d.label))
             .enter()
@@ -118,6 +120,9 @@ export const Doughnut = () => {
             .attr('fill', 'white')
             .attr('y', d => yScale(d3.sort(data, (a, b) => d3.ascending(a.price, b.price)).map(d => d.label).indexOf(d)))
             .attr('x', '2em')
+            .append('tspan')
+            .text(d => `${data.filter(element => element.label == d)[0].price}%`)
+            .attr('dx', '1em')
         labels
             .selectAll('circle')
             .data(d3.sort(data, (a, b) => d3.ascending(a.price, b.price)).map(d => d.label))
@@ -135,7 +140,7 @@ export const Doughnut = () => {
     }, [data])
     return (<>
         <div>
-            <h4>This week revenue:</h4>
+            <h4>This week organization:</h4>
             {/* <p>{data.reduce((a, e) => a + e.price, 0)}€</p> */}
         </div>
         <div ref={svgDiv}>
