71c6153e0ff1635a83a504439c3deec52c7489be
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2024-01-02 22:20:19 +0000

    repomarket

--- src/api/Repositories/market_repository.py
+++ src/api/Repositories/market_repository.py
@@ -5,15 +5,28 @@ from engine import session
 class MarketRepository:
     @staticmethod
     def get_market_list():
-        return
+        query = session.query(Market).all()
+        return query
+        
     
     @staticmethod
     def get_market_by_id():
-        return
+        query = session.query(Market).filter(Market.id == id)
+        return query
     
     @staticmethod
-    def add_market():
-        return
+    def add_market(market_data):
+        market_to_be_added = Market(
+            name=market_data["name"],
+            region=market_data["region"],
+            headquarters=market_data["headquarters"],
+            currency=market_data["currency"],
+            opentime=market_data["opentime"],
+            closetime=market_data["closetime"],
+        )
+        session.add(market_to_be_added)
+        session.commit()
+        return True
     
     @staticmethod
     def update_market():
@@ -21,4 +34,8 @@ class MarketRepository:
     
     @staticmethod
     def delete_market():
-        return
+        user_to_be_deleted = session.query(User).filter(User.id == user_data['id'])
+        session.delete(user_to_be_deleted)
+        session.commit()
+        return True
+        
