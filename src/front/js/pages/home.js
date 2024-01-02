3e5b3079ce9f17008de14ff19855a6372e2f271a
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-02 10:44:23 +0000

    Tweaks to Login animation

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,25 +3,38 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import '../../styles/shared.sass'
+import { useSpring, animated } from '@react-spring/web'
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
+	const springs = useSpring({
+		from: { opacity: 0 },
+		to: [{ opacity: 1 }],
+		config: {
+			duration: 1500
+		},
+	})
+
 	return (
 		<div className="text-center navbar-margin">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+			<animated.div
+				style={{
+					...springs,
+				}}
+			>
+				<h1>Hello Rigo!!</h1>
+				
+				<div className="alert alert-info">
+					{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+				</div>
+				<p>
+					This boilerplate comes with lots of documentation:{" "}
+					<a href="https://start.4geeksacademy.com/starters/react-flask">
+						Read documentation
+					</a>
+				</p>
+			</animated.div>
 		</div>
 	);
 };
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -3,19 +3,36 @@ import { YellowContainer } from "../component/color_containers/yellow_container"
 import { PurpleContainer } from "../component/color_containers/purple_container"
 import '../../styles/login.sass'
 
+import { useSpring, animated } from '@react-spring/web'
+
 export const Login = () => {
+
+    const springs = useSpring({
+        from: 
+            { x: 0, opacity: 0 },
+        to: { x: 50, opacity: 1 },
+        config: {
+            duration: 1500
+        },
+    })
     return (<>
         <div className="navbar-margin">
-            <h1>Login Page</h1>
-            <YellowContainer>
-                <h1>este sera el componente de login</h1>
-            </YellowContainer>
-            <PurpleContainer>
-                <input type="text" placeholder="email" />
-                <input type="password" name="password" id="password" placeholder="Password" />
-                <button>Login</button>
-                <p>Register</p>
-            </PurpleContainer>
+            <animated.div
+                style={{
+                    ...springs,
+                }}
+            >
+                <h1>Login Page</h1>
+                <YellowContainer>
+                    <h1>este sera el componente de login</h1>
+                </YellowContainer>
+                <PurpleContainer>
+                    <input type="text" placeholder="email" />
+                    <input type="password" name="password" id="password" placeholder="Password" />
+                    <button>Login</button>
+                    <p>Register</p>
+                </PurpleContainer>
+            </animated.div>
         </div>
     </>)
 } 
