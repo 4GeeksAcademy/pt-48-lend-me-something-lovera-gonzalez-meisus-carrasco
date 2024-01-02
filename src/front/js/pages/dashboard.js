c65f8eb02b49856e981856673cf7e18317c5aff3
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-02 10:32:43 +0000

    Added animation

--- src/front/js/pages/dashboard.js
+++ src/front/js/pages/dashboard.js
@@ -8,56 +8,72 @@ import { TestComponent } from "../component/test";
 import { TopBar } from "../component/top_searchbar";
 import "../../styles/dashboard.sass"
 import { Doughnut } from "../component/dashboard/doughnut_chart";
+import { useSpring, animated } from '@react-spring/web'
 
 export const Dashboard = () => {
+
+    const springs = useSpring({
+        from: { opacity: 0 },
+        to: [{ opacity: 1 }],
+        config: {
+            duration: 1500
+        },
+    })
+
+
     return (<>
         <div className="d-flex flex-column gap-5 navbar-margin">
             <TopBar />
-            <div className="d-flex flex-row justify-content-between flex-wrap gap-2 p-4 pt-0" style={{ width: '100%' }}>
-                <PurpleContainer style={{ width: '25%', flex: '1 0 auto' }}>
-                    <div className="text-light d-flex flex-row gap-2 justify-content-center align-items-center">
-                        <img src='https://restyler.s3.ap-southeast-2.amazonaws.com/user-data/user-46/artworks/product-79/preview/thumbnails/5MPgTBYjDfQKQP7gMsmbzC8lmELrSQWwHILNnnfr-watermarked-lg-resized.gif' alt="" style={{ height: '150px', filter: 'hue-rotate(100deg)' }} />
-                        <div>
-                            <h1>FF. Never Forfait.</h1>
-                            <h5>Go to the Moon</h5>
-                            <h2>Let your Finance Flow</h2>
-                            <h5>And come back as NEW man</h5>
+            <animated.div
+                style={{
+                    ...springs,
+                }}
+            >
+                <div className="d-flex flex-row justify-content-between flex-wrap gap-2 p-4 pt-0" style={{ width: '100%' }}>
+                    <PurpleContainer style={{ width: '25%', flex: '1 0 auto' }}>
+                        <div className="text-light d-flex flex-row gap-2 justify-content-center align-items-center">
+                            <img src='https://restyler.s3.ap-southeast-2.amazonaws.com/user-data/user-46/artworks/product-79/preview/thumbnails/5MPgTBYjDfQKQP7gMsmbzC8lmELrSQWwHILNnnfr-watermarked-lg-resized.gif' alt="" style={{ height: '150px', filter: 'hue-rotate(100deg)' }} />
+                            <div>
+                                <h1>FF. Never Forfait.</h1>
+                                <h5>Go to the Moon</h5>
+                                <h2>Let your Finance Flow</h2>
+                                <h5>And come back as NEW man</h5>
+                            </div>
                         </div>
-                    </div>
-                </PurpleContainer>
-                <BlueContainer style={{ width: '25%', flex: '1 0 auto' }}>
-                    <Doughnut />
-                </BlueContainer>
-                <PinkContainer style={{ width: '25%', flex: '1 0 auto' }} >
-                    <TestComponent />
-                </PinkContainer>
-                <GreenContainer style={{ width: '25%', flex: '1 0 auto' }}>
-                    <TestComponent />
-                </GreenContainer>
-            </div>
-            <div className="d-flex flex-row justify-content-between align-items-center p-4">
-                <PurpleContainer>
-                    <div style={{ width: '90%' }}>
-                        <h4>Prueba de texto componente largo</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nunc lectus,
-                            elementum sed magna et, malesuada condimentum quam. Nam posuere dolor et enim
-                            blandit lobortis. Donec cursus felis ac lectus hendrerit malesuada et imperdiet
-                            mi. Aenean fringilla suscipit nisl et ultricies. Nam quis nisi sollicitudin,
-                            imperdiet ante vel, elementum elit. Donec aliquam quam nec aliquam pellentesque.
-                            Nunc ac ligula semper felis mollis vestibulum et at sapien.
+                    </PurpleContainer>
+                    <BlueContainer style={{ width: '25%', flex: '1 0 auto' }}>
 
-                        </p>
-                    </div>
-                </PurpleContainer>
-                <YellowContainer>
-                    <div style={{ width: '90%' }}>
-                        <h4>Prueba de texto componente largo</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nunc lectus, elementum sed magna et, malesuada condimentum quam. Nam posuere dolor et enim blandit lobortis. Donec cursus felis ac lectus hendrerit malesuada et imperdiet mi. Aenean fringilla suscipit nisl et ultricies. Nam quis nisi sollicitudin, imperdiet ante vel, elementum elit. Donec aliquam quam nec aliquam pellentesque. Nunc ac ligula semper felis mollis vestibulum et at sapien.
 
-                        </p>
-                    </div>
-                </YellowContainer>
-            </div>
+                        <Doughnut />
+                    </BlueContainer>
+                    <PinkContainer style={{ width: '25%', flex: '1 0 auto' }} >
+                        <TestComponent />
+                    </PinkContainer>
+                </div>
+                <div className="d-flex flex-row justify-content-between align-items-center p-4">
+                    <PurpleContainer>
+                        <div style={{ width: '90%' }}>
+                            <h4>Prueba de texto componente largo</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nunc lectus,
+                                elementum sed magna et, malesuada condimentum quam. Nam posuere dolor et enim
+                                blandit lobortis. Donec cursus felis ac lectus hendrerit malesuada et imperdiet
+                                mi. Aenean fringilla suscipit nisl et ultricies. Nam quis nisi sollicitudin,
+                                imperdiet ante vel, elementum elit. Donec aliquam quam nec aliquam pellentesque.
+                                Nunc ac ligula semper felis mollis vestibulum et at sapien.
+
+                            </p>
+                        </div>
+                    </PurpleContainer>
+                    <YellowContainer>
+                        <div style={{ width: '90%' }}>
+                            <h4>Prueba de texto componente largo</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nunc lectus, elementum sed magna et, malesuada condimentum quam. Nam posuere dolor et enim blandit lobortis. Donec cursus felis ac lectus hendrerit malesuada et imperdiet mi. Aenean fringilla suscipit nisl et ultricies. Nam quis nisi sollicitudin, imperdiet ante vel, elementum elit. Donec aliquam quam nec aliquam pellentesque. Nunc ac ligula semper felis mollis vestibulum et at sapien.
+
+                            </p>
+                        </div>
+                    </YellowContainer>
+                </div>
+            </animated.div>
         </div>
     </>)
 }
