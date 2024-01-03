8d6c8b2caf23b19bf2917acf7524b61936c32cdb
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2024-01-03 09:11:59 +0000

    service and controller market added

---
+++ src/api/Services/market_service.py
@@ -0,0 +1,10 @@
+from api.Repositories.market_repository import MarketRepository
+from api.Services.HTTP_Status import HTTP_Status
+
+class MarketService:
+
+    @staticmethod
+    def get_list():
+        raw_data = MarketRepository.get_list()
+        serialized_data = list(map(lambda element: element.serialize(), raw_data))
+        return serialized_data, HTTP_Status.OK
