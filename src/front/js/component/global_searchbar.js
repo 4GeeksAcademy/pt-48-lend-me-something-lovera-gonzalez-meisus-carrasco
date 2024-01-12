1ec14420247b9538167a5c23f0507431374ce752
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2024-01-12 19:32:36 +0000

    modified x-animation

--- src/front/js/component/global_searchbar.js
+++ src/front/js/component/global_searchbar.js
@@ -1,16 +1,27 @@
 import React from "react";
 import "../../styles/global_searchbar.sass"
+import { useSpring, animated } from '@react-spring/web'
+
 
 export const GlobalSearchBar = ({ style, handleClick }) => {
+    const springs = useSpring({
+        from: 
+            { x: -10, opacity: 0 },
+        to: { x: 0, opacity: 1 },
+        config: {
+            duration: 750
+        },
+    })
 
     return (<>
-        <div className="navbar-margin globalsearch" style={style}>
+        <animated.div className="navbar-margin globalsearch" style={{...style, ...springs}}> 
+
             <div className="d-flex flex-row justify-content-center align-items-center" style={{ marginBottom: "15em" }}>
 
                 <i className="fa-solid fa-magnifying-glass magnifying" style={{ "color": "#ffffff" }}></i>
                 <input className='search-input' type="text" name="search" id="search" placeholder="Search" />
                 <button className=" button-close-search" onClick={handleClick}><i class="fa-solid fa-xmark"></i></button>
             </div>
-        </div>
+        </animated.div>
     </>)
 }
