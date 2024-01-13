2d1d1f76ab5e5a359bd259ec0947531e87f0aae2
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2024-01-13 12:10:58 +0000

    autocomplte in progress

--- src/front/js/component/global_searchbar.js
+++ src/front/js/component/global_searchbar.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "../../styles/global_searchbar.sass"
 import { useSpring, animated } from '@react-spring/web'
+import { element } from "prop-types";
 
 const listas = ["Home", "Dashboard", "Profile", "About Us"]
 
@@ -14,24 +15,45 @@ export const GlobalSearchBar = ({ style, handleClick }) => {
         },
     })
     const [query, setQuery] = useState('')
-    const [] = useState()
+    const [autoCompletado, setAutoCompletado] = useState('')
+    const [listaFiltrada, setListaFiltrada] = useState([])
+    const inputRef = useRef()
+
+    const handleChange = (e) => {
+        setQuery(e.target.value)
+        setListaFiltrada(listas.filter((lista) => lista.toLowerCase().includes(e.target.value.toLowerCase())))
+        const [firstelement] = listas.filter((lista) => lista.toLowerCase().includes(e.target.value.toLowerCase()))
+        console.log(firstelement)
+        setAutoCompletado(firstelement);
+        if (query != firstelement) {
+            const queryLength = e.target.value.length
+            inputRef.current.value = firstelement;
+            inputRef.current.setSelectionRange(queryLength, firstelement?.length)
+        }
+
+    }
+
+    // useEffect(() => {
+
+
+    // }, [query]) 
     return (<>
-        <animated.div className="navbar-margin globalsearch d-flex flex-column justify-content-start" style={{ ...style, ...springs }}>
+        <animated.div className="navbar-margin globalsearch flex-column justify-content-start" style={{ ...style, ...springs }}>
 
             <div className="d-flex flex-row justify-content-center align-items-center" style={{ marginTop: "10em" }}>
 
                 <i className="fa-solid fa-magnifying-glass magnifying" style={{ "color": "#ffffff" }}></i>
-                <input className='search-input' autoComplete="off" type="text" name="search" id="search" placeholder="Search" onChange={(e) => setQuery(e.target.value)} />
-                <button className=" button-close-search" onClick={handleClick}><i class="fa-solid fa-xmark"></i></button>
+                <input className='search-input' autoComplete="off" type="text" name="search" id="search" placeholder="Search" value={query} onChange={(e) => handleChange(e)} ref={inputRef} />
+                <button className=" button-close-search" onClick={handleClick}><i className="fa-solid fa-xmark"></i></button>
 
             </div>
             <div className=" ">
 
-                <ul style={query != "" ? {display:"unset"} : {display:"none"}} >
-                    {listas.filter((lista) => lista.toLocaleLowerCase().includes(query)).map((lista) => (
-                        <li>{lista}</li>
+                {listaFiltrada.length > 0 && <ul style={query != "" ? { display: "unset" } : { display: "none" }} >
+                    {listaFiltrada.map((element, index) => (
+                        <li key={index}>{element}</li>
                     ))}
-                </ul>
+                </ul>}
             </div>
 
         </animated.div>
