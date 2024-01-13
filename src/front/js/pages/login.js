164648dbdabcd1ec48a103cc843205531c6607b4
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-13 16:25:05 +0000

    Removed profile edits

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -28,19 +28,7 @@ export const Login = () => {
                     ...springs,
                 }} className='d-flex flex-column justify-content-center align-items-center gap-5'
             >
-                <h1>Profile Page</h1>
-                <YellowContainer>
-                    <h1>este sera el componente de Profile</h1>
-                </YellowContainer>
-                {isAuthenticated && user && <PurpleContainer>
-                    <div className="d-flex flex-column justify-content-center align-item-center border" style={{ width: '80vw' }}>
-                        <p>Nombre: </p> <p>{user.name}</p>
-                        <p>Apellido: </p> <p>{user.name}</p>
-                        <p>Dirección: </p> <p>{user.address}</p>
-                        <p>Teléfono: </p> <p>{}</p>
-                        <p>Email: </p> <p>{user.email}</p>
-                    </div>
-                </PurpleContainer>}
+                
             </animated.div>
         </div>
     </>)
