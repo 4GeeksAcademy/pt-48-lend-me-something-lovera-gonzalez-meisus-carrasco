21caa50696bdbecc1e48f6bdb7eee2e15f66e14d
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2024-01-13 09:31:04 +0000

    list added to searchbar

--- src/front/js/component/global_searchbar.js
+++ src/front/js/component/global_searchbar.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/global_searchbar.sass"
 import { useSpring, animated } from '@react-spring/web'
 
+const listas = ["Home", "Dashboard", "Profile", "About Us"]
 
 export const GlobalSearchBar = ({ style, handleClick }) => {
     const springs = useSpring({
@@ -12,16 +13,22 @@ export const GlobalSearchBar = ({ style, handleClick }) => {
             duration: 750
         },
     })
-
+    const [query, setQuery]= useState('')
     return (<>
         <animated.div className="navbar-margin globalsearch" style={{...style, ...springs}}> 
 
             <div className="d-flex flex-row justify-content-center align-items-center" style={{ marginBottom: "15em" }}>
 
                 <i className="fa-solid fa-magnifying-glass magnifying" style={{ "color": "#ffffff" }}></i>
-                <input className='search-input' type="text" name="search" id="search" placeholder="Search" />
+                <input className='search-input' type="text" name="search" id="search" placeholder="Search" onChange={(e)=> setQuery(e.target.value)}/>
                 <button className=" button-close-search" onClick={handleClick}><i class="fa-solid fa-xmark"></i></button>
+                <ul>
+                    {listas.filter((lista) => lista.toLocaleLowerCase().includes(query)).map((lista)=> (
+                    <li>{lista}</li>   
+                    ))}
+                </ul>
             </div>
+           
         </animated.div>
     </>)
 }
