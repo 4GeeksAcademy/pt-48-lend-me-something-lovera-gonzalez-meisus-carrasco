b08faa89284a0fc023611ab67b744d7ea19a0663
juan meisus <130049281+juanmeisus@users.noreply.github.com>
2024-01-13 16:20:54 +0000

    added auth0

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,14 +1,16 @@
 import React, { useContext } from "react";
 import { YellowContainer } from "../component/color_containers/yellow_container";
-import { PurpleContainer } from "../component/color_containers/purple_container"
-import '../../styles/login.sass'
+import { PurpleContainer } from "../component/color_containers/purple_container";
+import '../../styles/login.sass';
+import foto from "../../img/foto.jpg"
+
 
 import { useSpring, animated } from '@react-spring/web'
 
 export const Login = () => {
 
     const springs = useSpring({
-        from: 
+        from:
             { x: -10, opacity: 0 },
         to: { x: 0, opacity: 1 },
         config: {
@@ -16,23 +18,64 @@ export const Login = () => {
         },
     })
     return (<>
-        <div className="navbar-margin">
+        <div className="  navbar-margin " style={{ 'gap': '6em' }}>
             <animated.div
                 style={{
                     ...springs,
                 }}
             >
-                <h1>Login Page</h1>
-                <YellowContainer>
-                    <h1>este sera el componente de login</h1>
-                </YellowContainer>
-                <PurpleContainer>
-                    <input type="text" placeholder="email" />
-                    <input type="password" name="password" id="password" placeholder="Password" />
-                    <button>Login</button>
-                    <p>Register</p>
-                </PurpleContainer>
+                <div className="d-flex flex-row aling-items-center justify-content-around">
+
+                    <YellowContainer>
+                        <div className="">
+                            <img style={{ width: '200px', height: '200px', objectFit: 'cover', borderRadius: '25%' }} src={foto} />
+                        </div>
+
+                    </YellowContainer>
+
+
+                    <YellowContainer>
+
+                        <div className="">
+
+                            <h3>Info de contacto</h3>
+                            <p>Direccion</p>
+                            <p>Ubicacion</p>
+                            <p>Creacion de la cuenta</p>
+                            <p>Zona horaria</p>
+                            <p>Pais de residencia</p>
+                            <span>Recuerda nunca compartas tus claves, ni informacion acerca de tu cuenta</span>
+                            <p>El equipo de FlowFinance nunca solicitara tus password por sms o email</p>
+
+
+
+
+                        </div>
+                    </YellowContainer>
+                    <PurpleContainer>
+                        <h3>Detalles de la cuenta</h3>
+                        <div><p>Name:</p>
+                            <span>{ }</span>
+                        </div>
+                        <div><p>Lastname:</p>
+                            <span>{ }</span>
+                        </div>
+
+                        <div><p>Nickname:</p>
+                            <span>{ }</span>
+                        </div>
+
+                        <div><p>Created add:</p>
+                            <span>{ }</span>
+                        </div>
+
+
+
+
+                    </PurpleContainer>
+                </div>
             </animated.div>
         </div>
+        
     </>)
 } 
