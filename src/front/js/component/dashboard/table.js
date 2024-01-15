a5a46621f6c4dd3128050dc083b1570201f040a2
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2024-01-15 12:09:56 +0000

    chages added 2

--- src/front/js/component/dashboard/table.js
+++ src/front/js/component/dashboard/table.js
@@ -1,6 +1,6 @@
 import { AgGridReact } from 'ag-grid-react'; 
-import "ag-grid-community/dist/styles/ag-grid.css"; 
-import "ag-grid-community/dist/styles/ag-theme-quartz.css";
+import "ag-grid-community/styles/ag-grid.scss"; 
+import "ag-grid-community/styles/ag-theme-quartz.scss";
 import React, { useState } from 'react';
 
 export const Table = () => {
