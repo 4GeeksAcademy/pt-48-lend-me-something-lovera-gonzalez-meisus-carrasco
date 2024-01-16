afc8aeb383e9c9e0ea86d980ed726184396ef0f8
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-16 11:28:32 +0000

    Edited Color containers for style proping

--- src/front/js/component/color_containers/blue_container.js
+++ src/front/js/component/color_containers/blue_container.js
@@ -4,7 +4,7 @@ import "../../../styles/color_containers/blue_container.sass"
 export const BlueContainer = (props) => {
 
     return (<>
-        <div className="blue__container">
+        <div className="blue__container" style={{...props.style}}>
             {/* <p>Blue Container</p> */}
             {props.children}
         </div>
--- src/front/js/component/color_containers/pink_container.js
+++ src/front/js/component/color_containers/pink_container.js
@@ -4,7 +4,7 @@ import "../../../styles/color_containers/pink_container.sass"
 export const PinkContainer = (props) => {
 
     return (<>
-        <div className="pink__container" style={props.style}>
+        <div className="pink__container" style={{...props.style}}>
             {/* <h1>Pink Container</h1> */}
             {props.children}
         </div>
