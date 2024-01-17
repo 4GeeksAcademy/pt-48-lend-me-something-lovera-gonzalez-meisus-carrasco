1633dd0384d3f58564a06a84f4e48328c2a772de
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-17 20:24:56 +0000

    Dashboard with Auth0

--- src/front/js/pages/dashboard.js
+++ src/front/js/pages/dashboard.js
@@ -20,97 +20,99 @@ import { PDFViewer } from '@react-pdf/renderer';
 import { useAuth0 } from "@auth0/auth0-react";
 
 export const Dashboard = () => {
+    const { user, isLoading } = useAuth0();
 
-    const [searchState, setSearchState] = useState(false)
-    const { store, actions } = useContext(Context)
-    const { user } = useAuth0();
+    if (isLoading) {
 
-    const handleClick = () => {
+        return (<>
+            <div style={{width: '100%', height: '100%'}}>
+            <Spinner/>
 
-        setSearchState(!searchState);
-        console.log(searchState)
-    }
+            </div>
+        </>
+        )
+    } else
+        const [searchState, setSearchState] = useState(false)
+        // const { store, actions } = useContext(Context)
 
-    // useEffect(() => {
-    //     actions.setUser(user)
-    // }, [])
+        const handleClick = () => {
 
-    const springs = useSpring({
-        from: { opacity: 0, y: -5 },
-        to: [{ opacity: 1, y: 0 }],
-        config: {
-            mass: 5,
-            friction: 35,
-            tension: 120,
-        },
-    })
+            setSearchState(!searchState);
+            console.log(searchState)
+        }
 
+        useEffect(() => {
+            if (user) console.log(user)
+            // actions.setUser(user)
+        }, [])
 
-    return (<>
-        <TopBarTitle topTitle='Dashboard' />
-        <animated.div
-            style={{
-                ...springs,
-            }}
-            className="d-flex flex-column gap-5 navbar-margin"
-        >
-            <div className="d-flex flex-row justify-content-between flex-wrap gap-2 p-4 pt-0" style={{ width: '100%' }}>
-                <PurpleContainer >
-                    <div className="text-light d-flex flex-row gap-2 justify-content-center align-items-center">
-                        <img src='https://restyler.s3.ap-southeast-2.amazonaws.com/user-data/user-46/artworks/product-79/preview/thumbnails/5MPgTBYjDfQKQP7gMsmbzC8lmELrSQWwHILNnnfr-watermarked-lg-resized.gif' alt="" style={{ height: '150px', filter: 'hue-rotate(100deg)' }} />
-                        <div>
-                            <h1>FF. Never Forfait.</h1>
-                            <h5>Go to the Moon</h5>
-                            <h2>Let your Finance Flow</h2>
-                            <h5>And come back as NEW man</h5>
-                        </div>
+        const springs = useSpring({
+            from: { opacity: 0, y: -5 },
+            to: [{ opacity: 1, y: 0 }],
+            config: {
+                mass: 5,
+                friction: 35,
+                tension: 120,
+            },
+        })
 
-                    </div>
-
-                </PurpleContainer>
-
-
-
-                <BlueContainer>
-
-
-                    <Doughnut />
-                </BlueContainer>
-                {/* <PinkContainer  >
-                    <Spinner />
-                </PinkContainer> */}
-
-                <div className="d-flex flex-column gap-5 justify-content-between align-items-center p-4 " style={{ height: 700, width: '100%' }}>
-                    <Table />
-                <PDFViewer>
-                    <MyDocument />
-                </PDFViewer>
-                </div>
-
-                <div className="d-flex flex-row justify-content-between align-items-center p-4">
-                    <PurpleContainer>
-                        <div style={{ width: '90%' }}>
-                            <h4>Prueba de texto componente largo</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nunc lectus,
-                                elementum sed magna et, malesuada condimentum quam. Nam posuere dolor et enim
-                                blandit lobortis. Donec cursus felis ac lectus hendrerit malesuada et imperdiet
-                                mi. Aenean fringilla suscipit nisl et ultricies. Nam quis nisi sollicitudin,
-                                imperdiet ante vel, elementum elit. Donec aliquam quam nec aliquam pellentesque.
-                                Nunc ac ligula semper felis mollis vestibulum et at sapien.
-
-                            </p>
+        return (<>
+            <TopBarTitle topTitle='Dashboard' />
+            <animated.div
+                style={{
+                    ...springs,
+                }}
+                className="d-flex flex-column gap-5 navbar-margin"
+            >
+                <div className="d-flex flex-row justify-content-between flex-wrap gap-2 p-4 pt-0" style={{ width: '100%' }}>
+                    <PurpleContainer >
+                        <div className="text-light d-flex flex-row gap-2 justify-content-center align-items-center">
+                            <img src='https://restyler.s3.ap-southeast-2.amazonaws.com/user-data/user-46/artworks/product-79/preview/thumbnails/5MPgTBYjDfQKQP7gMsmbzC8lmELrSQWwHILNnnfr-watermarked-lg-resized.gif' alt="" style={{ height: '150px', filter: 'hue-rotate(100deg)' }} />
+                            <div>
+                                <h1>FF. Never Forfait.</h1>
+                                <h5>Go to the Moon</h5>
+                                <h2>Let your Finance Flow</h2>
+                                <h5>And come back as NEW man</h5>
+                            </div>
                         </div>
                     </PurpleContainer>
-                    <YellowContainer>
-                        <div style={{ width: '90%' }}>
-                            <h4>Prueba de texto componente largo</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nunc lectus, elementum sed magna et, malesuada condimentum quam. Nam posuere dolor et enim blandit lobortis. Donec cursus felis ac lectus hendrerit malesuada et imperdiet mi. Aenean fringilla suscipit nisl et ultricies. Nam quis nisi sollicitudin, imperdiet ante vel, elementum elit. Donec aliquam quam nec aliquam pellentesque. Nunc ac ligula semper felis mollis vestibulum et at sapien.
-
-                            </p>
-                        </div>
-                    </YellowContainer>
+                    <BlueContainer>
+                        <Doughnut />
+                    </BlueContainer>
+                    <div className="d-flex flex-column gap-5 justify-content-between align-items-center p-4 " style={{ height: 700, width: '100%' }}>
+                        <Table />
+                        <PDFViewer>
+                            <MyDocument />
+                        </PDFViewer>
+                    </div>
+                    <div className="d-flex flex-row justify-content-between align-items-center p-4">
+                        <PurpleContainer>
+                            <div style={{ width: '90%' }}>
+                                <h4>Prueba de texto componente largo</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nunc lectus,
+                                    elementum sed magna et, malesuada condimentum quam. Nam posuere dolor et enim
+                                    blandit lobortis. Donec cursus felis ac lectus hendrerit malesuada et imperdiet
+                                    mi. Aenean fringilla suscipit nisl et ultricies. Nam quis nisi sollicitudin,
+                                    imperdiet ante vel, elementum elit. Donec aliquam quam nec aliquam pellentesque.
+                                    Nunc ac ligula semper felis mollis vestibulum et at sapien.
+                                </p>
+                            </div>
+                        </PurpleContainer>
+                        <YellowContainer>
+                            <div style={{ width: '90%' }}>
+                                <h4>Prueba de texto componente largo</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nunc lectus, 
+                                    elementum sed magna et, malesuada condimentum quam. Nam posuere dolor et enim
+                                    blandit lobortis. Donec cursus felis ac lectus hendrerit malesuada et imperdiet
+                                    mi. Aenean fringilla suscipit nisl et ultricies. Nam quis nisi sollicitudin,
+                                    imperdiet ante vel, elementum elit. Donec aliquam quam nec aliquam
+                                    pellentesque. Nunc ac ligula semper felis mollis vestibulum et at sapien.
+                                </p>
+                            </div>
+                        </YellowContainer>
+                    </div>
                 </div>
-            </div>
-        </animated.div>
-    </>)
+            </animated.div>
+        </>)
+    }
 }
