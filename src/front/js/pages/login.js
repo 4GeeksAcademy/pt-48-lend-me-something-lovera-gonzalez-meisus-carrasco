1fa8c89c9cdac64afb92ddbe92155927dacd76c4
juan meisus <130049281+juanmeisus@users.noreply.github.com>
2024-01-17 22:37:51 +0000

    Profile actualizada

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { YellowContainer } from "../component/color_containers/yellow_container";
 import { PurpleContainer } from "../component/color_containers/purple_container";
 import { GreenContainer } from "../component/color_containers/green_container";
@@ -22,6 +22,13 @@ export const Login = () => {
             duration: 750
         },
     })
+
+    const [estadoEdicion, setEstadoEdicion] = useState(false);
+    const [ubicacion, setUbicacion] = useState('vigo');
+    const [direccion, setDireccion] = useState('Calle Anduriña');
+
+
+
     return (<>
         <TopBarTitle topTitle='My profile' />
 
@@ -32,61 +39,68 @@ export const Login = () => {
                 }} className='d-flex flex-column justify-content-center align-items-center gap-5'
             >
                 <div className="d-flex flex-row align-items-center justify-content-around">
-
-
-
                     <GreenContainer style={{ position: 'relative', width: '500px' }}>
                         <div className="profile-picture-container">
                             <YellowContainer style={{ transform: 'rotate(180deg)' }}>
                                 <div className="">
                                     <img style={{ width: '150px', height: '150px', objectFit: 'cover', borderRadius: '25%', transform: 'rotate(-180deg)' }} src={user.picture} />
                                 </div>
-
                             </YellowContainer>
                         </div>
-
-                        <div className="">
-
+                        <div className="profile-info ">
                             <h3>Info de contacto</h3>
-                            <p>Direccion</p>
-                            <p>Ubicacion</p>
-                            <p>Creacion de la cuenta</p>
-                            <p>Zona horaria</p>
-                            <p>Pais de residencia</p>
+                            <p  >Direccion :
+                                {!estadoEdicion && <span>{direccion}</span>}
+                                {estadoEdicion && (
+                                    <input className="profile-input" onChange={(e) => setDireccion(e.target.value)} value={direccion} type="text"></input>
+                                )}
+                            </p>
+                            <p>Ubicacion :
+                                {!estadoEdicion && <span>{ubicacion}</span>}
+                                {estadoEdicion && (
+                                    <input className="profile-input" onChange={(e) => setUbicacion(e.target.value)} value={ubicacion} type="text"></input>
+                                )}
+                            </p>
+                            <p>Creacion de la cuenta :
+                                <span>2024</span>
+                            </p>
+                            <p>Zona horaria :
 
-
-
-
-
+                                <span>europa</span>
+                            </p>
+                            <p>Pais de residencia :
+                                <span>españa</span></p>
+                                <BlueContainer>
+                            <button className="profile-button "onClick={() => setEstadoEdicion(!estadoEdicion)}>
+                                {estadoEdicion ? 'guardar' : 'editar'}</button>
+                                </BlueContainer>
                         </div>
                     </GreenContainer>
                     <PurpleContainer>
                         <h3>Detalles de la cuenta</h3>
-                        <div><p>Name:</p>
-                            <span>{user.given_name}</span>
+                        <div><p>Name:
+
+                            <span>{user.given_name} </span>
+                        </p>
                         </div>
-                        <div><p>Lastname:</p>
-                            <span>{user.family_name}</span>
+                        <div><p>Lastname:
+
+                            <span>{user.family_name} </span>
+                        </p>
                         </div>
+                        <div><p>Nickname:
 
-                        <div><p>Nickname:</p>
-                            <span>{user.nickname}</span>
+                            <span>{user.nickname} </span>
+                        </p>
                         </div>
-
-
-
-
-
-
                     </PurpleContainer>
                 </div>
-
-            <BlueContainer>
-                <span>Recuerda nunca compartas tus claves, ni informacion acerca de tu cuenta</span>
-                <p>El equipo de FlowFinance nunca solicitara tus password por sms o email</p>
-            </BlueContainer>
-            </animated.div>
-        </div>
-
+                <BlueContainer>
+                    <span>Recuerda nunca compartas tus claves, ni informacion acerca de tu cuenta</span>
+                    <p>El equipo de FlowFinance nunca solicitara tus password por sms o email</p>
+                </BlueContainer>
+            </animated.div >
+        </div >
     </>)
-} 
+}
+
