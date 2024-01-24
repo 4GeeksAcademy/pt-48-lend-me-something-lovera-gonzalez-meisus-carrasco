609bf7963eb9db69c8146ac439754a37c7cc4c15
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-24 13:47:00 +0000

    Created FlowFinance API file for FrontEnd

---
+++ src/front/js/store/flowfinance_api.js
@@ -0,0 +1,20 @@
+const FF_API = process.env.BACKEND_URL
+
+export class FF_APi {
+    static {
+        const getUser = () => { };
+        const edditUser = () => { };
+        const deleteUser = () => { };
+
+
+        const getSubscription = () => { };
+        const updateSubscription = () => { };
+
+
+        const getPortfolio = () => { };
+        const addToPortfolio = () => { };
+        const deleteFromPortfolio = () => { };
+
+    };
+};
+
