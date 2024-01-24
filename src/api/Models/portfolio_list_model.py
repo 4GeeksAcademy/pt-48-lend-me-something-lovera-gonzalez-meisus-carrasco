e786541030c1d62e8970747dc112dd022bfed9e8
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-24 14:09:07 +0000

    Modified PorfolioList model

--- src/api/Models/portfolio_list_model.py
+++ src/api/Models/portfolio_list_model.py
@@ -1,10 +1,18 @@
 from api.models import db
 
+from enum import Enum
 
+class ItemType(Enum):
+    STOCK = 'Stock'
+    CRYPTO = 'Crypto'
+    FOREX = 'Forex'
+    COMMODITY = 'Commodity'
 class PortfolioList(db.Model):
     __tablename__ = 'portfolios'
     id = db.Column(db.Integer, nullable =False, primary_key = True)
     portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.id'))
+    item_type = db.Column(db.Enum(ItemType), nullable = False)
+    item_symbol = db.Column(db.String, nullable=False) 
 
     portfolio = db.relationship('Portfolio', back_populates='portfolio_list')
 
@@ -15,5 +23,7 @@ class PortfolioList(db.Model):
     def serialize (self):
         return {
             'id' : self.id,
-            'portfolio_id' : self.portfolio_id
+            'portfolio_id' : self.portfolio_id,
+            'item_type': self.item_type,
+            'item_symbol': self.item_symbol
         }
