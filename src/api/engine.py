18f94346e6311aedf5d9000316252cab8fb4cf19
Hans Aparicio <hansaparicio@gmail.com>
2024-01-25 22:41:42 +0000

    render

--- src/api/engine.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from api.models import db
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine("postgresql://gitpod:postgres@localhost:5432/example", echo=True)
-db.Model.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
