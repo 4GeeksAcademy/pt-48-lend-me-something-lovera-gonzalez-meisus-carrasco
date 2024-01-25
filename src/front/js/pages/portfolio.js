5a84969522629128ee8c42222c47b01e34b8a62b
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2024-01-25 17:45:50 +0000

    portfolio files added

---
+++ src/front/js/pages/portfolio.js
@@ -0,0 +1,20 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from '../store/appContext.js'
+import { BlueContainer } from "../component/color_containers/blue_container";
+import { GreenContainer } from "../component/color_containers/green_container";
+import { PinkContainer } from "../component/color_containers/pink_container";
+import { YellowContainer } from "../component/color_containers/yellow_container";
+import { PurpleContainer } from "../component/color_containers/purple_container";
+import "../../styles/dashboard.sass"
+import { useSpring, animated } from '@react-spring/web'
+import { Spinner } from "../component/spinner"
+import { TopBarTitle } from "../component/topBarTitle.js";
+
+
+export const Portfolio = () => {
+
+    return(<>
+    
+    
+    </>)
+}
