542d545ed24dfefc03d998c0d436d47971086ec6
Hans Aparicio <hansaparicio@gmail.com>
2024-01-26 15:56:14 +0000

    render

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,7 +1,6 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom";
-
+import { createRoot } from 'react-dom/client';
 //include your index.scss file into the bundle
 import "../styles/index.css";
 
@@ -9,4 +8,7 @@ import "../styles/index.css";
 import Layout from "./layout";
 
 //render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Layout />);
