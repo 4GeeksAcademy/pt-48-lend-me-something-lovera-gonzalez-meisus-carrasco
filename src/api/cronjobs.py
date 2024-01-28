b9e143a160c1ac457e029b8914665002b4c8b4d2
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-28 20:10:42 +0000

    modified test.py cronjob

--- src/api/cronjobs.py
+++
@@ -1,6 +0,0 @@
-# from crontab import CronTab
-
-# cron = CronTab(user="root")
-# job = cron.new(command="echo hello_world")
-# job.minute.every(1)
-# cron.write()
--- src/api/test.py
+++ src/api/test.py
@@ -9,5 +9,25 @@ with open(
 ) as f:
     f.write("The date is {}\n".format(now))
 
+symbols = "eurusd,gbpusd,usdjpy,audusd,gbpjpy,usdcad,eurjpy,usdchf,eurgbp,gbpaud,nzdusd,euraud,audjpy,cadjpy,gbpcad,eurcad,audcad,chfjpy,eurnzd,gbpnzd,eurchf,audchf,gbpchf,audnzd,cadchf,nzdjpy,nzdcad,nzdchf,usdzar,usdmxn,usdcnh,usdtry,usdsek,eursek,usdhkd,usdnok,eurnok,eurtry,mxnjpy,eurhuf,zarjpy,usdhuf,tryjpy"
+
+headers = {"Content-Type": "application/json"}
+requestResponse = requests.get(
+    f"https://api.tiingo.com/tiingo/fx/top?tickers={symbols}&token=d29461d6a952547a6a6c50e5253276f740fe2fd8",
+    headers=headers,
+)
+print(requestResponse.json())
+
+delete = requests.delete(
+    "https://studious-space-sniffle-jjpp6wvv5wfj7q6-3001.app.github.dev/forex"
+)
+
+print(delete)
+add = requests.post(
+    "https://studious-space-sniffle-jjpp6wvv5wfj7q6-3001.app.github.dev/forex",
+    data=requestResponse,
+    headers=headers,
+)
 
 
+print(add)
