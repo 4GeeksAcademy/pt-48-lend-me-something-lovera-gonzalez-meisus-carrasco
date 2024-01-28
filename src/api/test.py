93555fe9f44b5e07314c4f0ce1351df817681732
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-28 18:04:22 +0000

    more test

--- src/api/test.py
+++ src/api/test.py
@@ -3,7 +3,7 @@ from datetime import date
 
 now = date.today()
 with open(
-    "/workspaces/pt-48-lend-me-something-lovera-gonzalez-meisus-carrasco/src/api/test.txt",
+    "src/api/test.txt",
     "a",
 ) as f:
     f.write("The date is {}\n".format(now))
