f118444eb288d608f432d5cb89f6f9ed04f9dab8
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-28 17:07:06 +0000

    test.py

--- src/api/test.py
+++ src/api/test.py
@@ -1,1 +1,9 @@
-print('Hello from test')
+import random
+from datetime import date
+
+now = date.today()
+with open(
+    "/workspaces/pt-48-lend-me-something-lovera-gonzalez-meisus-carrasco/src/api/test.txt",
+    "a",
+) as f:
+    f.write("The date is {}\n".format(now))
