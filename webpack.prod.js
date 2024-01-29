9f69165a2156c6fb0ef804d8aacf63748f87fd8d
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-29 10:46:21 +0000

    changes to webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,12 @@ const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
 module.exports = merge(common, {
     mode: 'production',
+    entry:
+        './src/front/js/index.js'
+    ,
     output: {
+
+        filename: 'bundle.js',
         publicPath: '/'
     },
     plugins: [
