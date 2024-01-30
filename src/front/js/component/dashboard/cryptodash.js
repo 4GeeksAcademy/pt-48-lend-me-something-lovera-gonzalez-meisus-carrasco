2c6ce983595009cdc8e65fec5e678f2352514726
Augusto Carrasco <augustocarrascotoro@gmail.com>
2024-01-30 17:33:29 +0000

    Changes to display user portfolio on Crypto

--- src/front/js/component/dashboard/cryptodash.js
+++ src/front/js/component/dashboard/cryptodash.js
@@ -14,6 +14,7 @@ import { SmallTable } from "./search_table.js";
 export const CrryptoDash = () => {
 
     const { store, actions } = useContext(Context)
+    const [graphData, setGraphData] = useState();
 
 
     const filtered_data = store?.cryptoDB?.map(element => ({
@@ -36,12 +37,30 @@ export const CrryptoDash = () => {
             friction: 35,
             tension: 120,
         },
-    })
+    });
+    let portfolioSize;
 
-    useEffect(() => {
+
+    const loadData = async () => {
+        portfolioSize = store.userPortfolio?.filter(element => element.item_type === 'Crypto').length > 0
+        // console.log(portfolioSize)
+        if (portfolioSize) {
+            const symbols = store.userPortfolio?.filter(element => element.item_type === 'Crypto')
+            // console.log(symbols)
+            const data = symbols.reduce((acc, next) => [...acc, store.cryptoDB.filter(element=> element.asset_id === next.item_symbol)[0]],[])
+            console.log(data)
+            setGraphData(await data.map(e => ({ price: e.price_usd != null ?  e.price_usd : 1, name: e.asset_id })).slice(0, 10))
+        }
+
         setTimeout(() => {
             setLoading(false)
+
         }, 1000)
+
+    }
+
+    useEffect(() => {
+        loadData()
         return () => {
             setTableColumns()
             setData()
@@ -62,7 +81,7 @@ export const CrryptoDash = () => {
 
                 <div className="d-flex flex-row justify-content-between align-items-center gap-4" style={{ width: '80%' }}>
                     <BlueContainer style={{ alignItems: 'center', justifyItems: 'center', }}>
-                        <Doughnut data={filtered_data.splice(0, 10)} colors={['#5F8670', '#FF9800', '#B80000', '#820300']} title='Top 10 Cryptos!' />
+                        <Doughnut data={graphData} colors={['#5F8670', '#FF9800', '#B80000', '#820300']} title='Top 10 Cryptos!' />
                     </BlueContainer>
                     <BlueContainer>
                         <SmallTable data={store?.cryptoDB?.slice(0,500).map(e=> ({name: e.name, symbol: e.asset_id}))} title='Cryptocurrencies' type='Crypto'/>
